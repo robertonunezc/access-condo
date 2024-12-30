9eb574e3e1e230d4a24bf62f2e35a069c17d10c7
Roberto Nuñez <rcorralesn@gmail.com>
2024-12-30 08:15:29 -0500

    Update database connection configuration: modify host, user, password, and database name defaults

--- db/knex.ts
+++ db/knex.ts
@@ -1,15 +1,13 @@
 import { Knex } from 'knex';
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env' }); // Load environment variables from .env file
 console.log("DB",process.env.DATABASE_HOST);
 const knexConfig: Knex.Config = {
     client: 'pg',
     connection: {
-      host: process.env.DATABASE_HOST || 'db',
+      host: process.env.DATABASE_HOST || 'dbaa',
       port: parseInt(process.env.DATABASE_PORT as string) || 5432,
-      user: process.env.DATABASE_USER || 'condo',
-      password: process.env.DATABASE_PASSWORD || 'condo123.',
-      database: process.env.DATABASE_NAME || 'condo',
+      user: process.env.DATABASE_USER || 'condooo',
+      password: process.env.DATABASE_PASSWORD || 'condo1232.',
+      database: process.env.DATABASE_NAME || 'cond33o',
     },
     migrations: {
         tableName: 'migrations',
